b0b5b55d2c42a0166fab5365047a0543bbef2ca9
yongkai-mac <keanu_kai@outlook.com>
2026-02-24 11:10:19 +0800

    [ADD] added NSFC

---
+++ 2026青年基金/compress.sh
@@ -0,0 +1,4 @@
+echo "Compressing";
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile=2021_ChenScalo_JFM-DISTILLED.pdf 2021_ChenScalo_JFM.pdf
+
+
