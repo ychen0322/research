44fc1e7318b1de7d0ac4dd7483eec6c6438c39cc
yongkai-mac <keanu_kai@outlook.com>
2026-03-11 16:06:10 +0800

    [MOD] NSFC

--- 2026青年基金/代表作/compress.sh
+++ 2026青年基金/代表作/compress.sh
@@ -1,3 +1,15 @@
 echo "Compressing";
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile=JFM_cone_compressed.pdf JFM_cone.pdf
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook \
+-dColorImageResolution=110 \
+   -dGrayImageResolution=110 \
+   -dMonoImageResolution=300 \
+   -dOptimize=true \
+   -dDownsampleColorImages=true \
+   -dDownsampleGrayImages=true \
+   -dDownsampleMonoImages=true \
+   -dColorImageDownsampleType=/Average \
+   -dDetectDuplicateImages=true \
+   -dEmbedAllFonts=false \
+   -dSubsetFonts=true \
+-dNOPAUSE -dQUIET -dBATCH -sOutputFile=25-QTC-compressed.pdf 25-QTC.pdf
 
